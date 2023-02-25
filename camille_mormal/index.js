bf04b120cc5f84532aad869be6ac5531804fe8c8
Amal jude Joseph <95752533+Amaljjaaaaa@users.noreply.github.com>
2023-02-25 18:32:20 +0530

    Update index.js

--- camille_mormal/index.js
+++ camille_mormal/index.js
@@ -1,10 +1,10 @@
 const track = document.getElementById("image-track");
-let sliderInterval;
+
 
 window.onmousedown = e => {
   track.dataset.mouseDownAt = e.clientX;
-  clearInterval(sliderInterval); // Stop any previously running slider interval
-  e.preventDefault(); // Prevent default behavior of the browser when dragging images
+  
+  e.preventDefault(); 
 }
 
 window.onmousemove = e => {
@@ -32,7 +32,5 @@ window.onmousemove = e => {
 window.onmouseup = () => {
   track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
-  sliderInterval = setInterval(() => { // Start the slider interval again
-    // sliding animation code goes here
-  }, 1000);
+  
 }
